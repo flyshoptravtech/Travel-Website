624d4caa811e1fe3425f1ea318afe8b39a57e6a8
Dipanshu Bisht <dipanshu.flyshop@gmail.com>
2024-07-23 19:02:37 +0530

    Changes in mobile login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,27 @@ const Header = ({navLinks,activeLink,isFixed,width,mobNav,setmobNav,authToken,us
                 <div className="nav-toggle" onClick={setmobNav} />
                 <div className="mobile_nav">
                     <ul>
+                    {
+                        authToken === null ?
                         <li>
                             <a className="bg-light-primary text-primary rounded" data-bs-toggle="modal" data-bs-target="#login"><i className="fa-regular fa-circle-user fs-6" /></a>
                         </li>
+                        :
+                        <li className='dropdown'>
+                            <a className="bg-light-primary text-primary rounded" type='button' data-bs-toggle="dropdown" aria-expanded="false">
+                                <i className="fa-regular fa-circle-user fs-6" />
+                            </a>
+                            <ul className="dropdown-menu dropdown-menu-end">
+                                {
+                                    userLinks.map((item,index)=>(
+                                        <li className='w-100' key={index}>
+                                            <Link className={`d-block h-auto w-100 py-1 ${item.name === "Logout" ? "text-danger" :""}`} to={item.link} style={{whiteSpace:"nowrap"}} onClick={item.function || null} >{item.name}</Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </li>
+                    }
                     </ul>
                 </div>
                 </div>
