b62282ce60c3ed7560cd47dc913a8cc77f5eede7
Dipanshu Bisht <dipanshu.flyshop@gmail.com>
2024-07-27 15:45:17 +0530

    Changes

--- src/components/SearchInputBox.jsx
+++ src/components/SearchInputBox.jsx
@@ -1,67 +1,76 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Select from "react-select"
+import AsyncSelect from "react-select/async"
 
 const SearchInputBox = () => {
 
-    const {goingTo} = useParams()
-    const [city, setCity] = useState("")
-    const [CityOptions, setCityOptions] = useState([]);
-    const apiUrl = process.env.REACT_APP_API_URL;
+  const {goingTo} = useParams()
+  const [city, setCity] = useState("")
+  const [CityOptions, setCityOptions] = useState([]);
+  const apiUrl = process.env.REACT_APP_API_URL;
 
-    const selectBoxStyle = {
-        control:(base,state)=>({
-            ...base,outline:"none",boxShadow:state.isFocused ? "":"",width:"100%",height:"fit-content",padding:".8rem 0.3rem",border:"1px solid #dee2e6",borderRadius:".375rem",display:"flex",backgroundColor:"#fff",':hover':{border:'1px solid #dee2e6'}
-        }),
-        singleValue: (base) => ({
-            ...base,fontWeight: "600"
-        }),
-        input: (base) => ({
-            ...base,fontWeight: "600"
-        }),
-        dropdownIndicator:()=>({
-            display:"none",opacity:"0"
-        }),
-        placeholder:(base)=>({
-            ...base,fontSize:"1rem",fontWeight:"600",padding:"0",color:"#595c5f"
-        }),
-        indicatorSeparator:()=>({
-            backgroundColor:"transparent"
-        }),
-        clearIndicator:()=>({
-            backgroundColor:"transparent"
-        }),
-    }
+  const selectBoxStyle = {
+    control:(base,state)=>({
+      ...base,outline:"none",boxShadow:state.isFocused ? "":"",width:"100%",height:"fit-content",padding:".8rem 0.3rem",border:"1px solid #dee2e6",borderRadius:".375rem",display:"flex",backgroundColor:"#fff",':hover':{border:'1px solid #dee2e6'}
+    }),
+    singleValue: (base) => ({
+      ...base,fontWeight: "600"
+    }),
+    input: (base) => ({
+      ...base,fontWeight: "600"
+    }),
+    dropdownIndicator:()=>({
+      display:"none",opacity:"0"
+    }),
+    placeholder:(base)=>({
+      ...base,fontSize:"1rem",fontWeight:"600",padding:"0",color:"#595c5f"
+    }),
+    indicatorSeparator:()=>({
+      backgroundColor:"transparent"
+    }),
+    clearIndicator:()=>({
+      backgroundColor:"transparent"
+    }),
+  }
   
-    const axiosHeaders = {
-        headers:{"Content-Type":"application/json",}
-    }
+  const axiosHeaders = {
+    headers:{"Content-Type":"application/json",}
+  }
   
-    useEffect(() => {
-        axios.get(`${apiUrl}cities`, axiosHeaders)
-          .then((response) => {
-            let cityOp = response.data.cities;
-            cityOp = cityOp.map((option) => ({
-              value: option.id,
-              label: option.name,
-            }));
-            setCityOptions(cityOp);
-            if (goingTo) {
-              const selectedCity = cityOp.find((option) => option.value === goingTo);
-              setCity(selectedCity || "");
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, [apiUrl, goingTo]);
+  useEffect(() => {
+    axios.get(`${apiUrl}cities`, axiosHeaders)
+    .then((response) => {
+      let cityOp = response.data.cities;
+      cityOp = cityOp.map((option) => ({
+        value: option.id,
+        label: option.name,
+      }));
+      setCityOptions(cityOp);
+        if (goingTo) {
+          const selectedCity = cityOp.find((option) => option.value === goingTo);
+          setCity(selectedCity || "");
+        }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    // eslint-disable-next-line
+  }, [apiUrl, goingTo]);
+
+  const loadOptions = (searchValue,callback)=>{
+    setTimeout(() => {
+      const filteredOptions  = CityOptions.filter(option=>option.label.toLowerCase().includes(searchValue.toLowerCase()))
+      callback(filteredOptions)
+    }, 100);
+  }
 
 
   return (
     <div>
-      <Select
-        options={CityOptions}
+      <AsyncSelect
+        loadOptions={loadOptions}
+        defaultOptions={CityOptions}
         placeholder="Going to"
         noOptionsMessage={() => "No City Found..."}
         styles={selectBoxStyle}
--- src/pages/affiliate/AffiliateForm.jsx
+++ src/pages/affiliate/AffiliateForm.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../layout/Layout'
+import animated from "react-select/animated"
+import Select from "react-select/creatable"
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const AffiliateForm = () => {
+
+    const [inputs, setInputs] = useState({ input1: '', input2: '' });
+    const [category, setcategory] = useState("")
+    const [loading, setloading] = useState(false)
+    const makeAnimated = animated()
+    const apiUrl = process.env.REACT_APP_API_URL
+    const navigate = useNavigate()
+
+    const options = [
+        {label:"Youtube",value:"youtube"},
+        {label:"Laura",value:"Jonathan"},
+    ]
+
+    const axiosHeaders = {
+        headers:{"Content-Type":"application/json",}
+    }
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        const regex = /^[0-9]*$/;
+
+        if (regex.test(value)) {
+            setInputs((prev) => ({ ...prev, [id]: value }));
+        }else{
+            e.target.value = inputs[id];
+        }
+    }
+
+    const affiliateSubmit = (e) =>{
+        e.preventDefault()
+        setloading(true)
+        const form = new FormData(e.target)
+        const data = Object.fromEntries(form.entries())
+        let stringCate = category
+        if(Array.isArray(stringCate)){
+            stringCate= category.map(item=>item.value).join(",")
+        }
+        const fullData = {...data,category:stringCate}
+        axios.post(`${apiUrl}affiliate-register`,fullData,axiosHeaders)
+        .then((res)=>{
+            toast.success(res.data.message);
+            navigate("/")
+            setloading(false)
+        })
+        .catch((err)=>{
+            setloading(false)
+            const error = JSON.parse(err.request.response).message
+            if(error.name){
+                toast.error(error.name[0])
+            }else if(error.email){
+                toast.error(error.email[0])
+            }else if(error.whatsapp_number){
+                toast.error(error.whatsapp_number[0])
+            }
+        })
+    }
+
   return (
     <Layout>
         <section className="py-5">
@@ -13,57 +75,69 @@ const AffiliateForm = () => {
                         <div className="col-12 order-1">
                         <div className="p-4 p-sm-7">
                             <h1 className="mb-2 fs-2">Join us as Influencer</h1>
-                            <form className="mt-3 text-start">
+                            <form className="mt-3 text-start" onSubmit={affiliateSubmit}>
                             <div className="form">
                                 <div className="form-group">
-                                <label className="form-label">Enter Name *</label>
-                                <input type="text" className="form-control" name='name' placeholder="Enter Your Name" autoFocus />
+                                    <label className="form-label">Enter Name *</label>
+                                    <input type="text" className="form-control" name='name' placeholder="Enter Full Name"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Enter Email *</label>
-                                <input type="email" className="form-control" name='email' placeholder="name@example.com" autoFocus />
+                                    <label className="form-label">Enter Email *</label>
+                                    <input type="email" className="form-control" name='email' placeholder="name@example.com"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Youtube Link <span style={{fontSize:'12px',fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
-                                <input type="text" className="form-control" name='email' placeholder="https://www.youtube.com/username" autoFocus />
+                                    <label className="form-label">Youtube Link <span style={{fontSize:'12px',   fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
+                                    <input type="text" className="form-control" name='youtube_link' placeholder="https://www.youtube.com/username"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Youtube Subscriber ( In Number ) :</label>
-                                <input type="number" className="form-control" name='email' placeholder="Youtube Subscriber" autoFocus />
+                                    <label className="form-label">Youtube Subscriber ( In Number ) :</label>
+                                    <input type="text" className="form-control" name='youtube_subscriber' placeholder="Youtube Subscriber" id='input1' onChange={handleChange}  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Instagram Link <span style={{fontSize:'12px',fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
-                                <input type="text" className="form-control" name='email' placeholder="https://www.instagram.com/username" autoFocus />
+                                    <label className="form-label">Instagram Link <span style={{fontSize:'12px',fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
+                                    <input type="text" className="form-control" name='instagram_link' placeholder="https://www.instagram.com/username"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Instagram Followers ( In Number ) :</label>
-                                <input type="text" className="form-control" name='email' placeholder="Instaram Followers" autoFocus />
+                                    <label className="form-label">Instagram Followers ( In Number ) :</label>
+                                    <input type="text" className="form-control" name='instagram_followers' placeholder="Instaram Followers" id='input2' onChange={handleChange}  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Other Link  <span style={{fontSize:'12px',fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
-                                <input type="text" className="form-control" name='email' placeholder="https://www.other.com/username" autoFocus />
+                                    <label className="form-label">Other Link  <span style={{fontSize:'12px',fontWeight:"600"}}>( Kindly copy the URL link of your profile from your browser and paste here )</span></label>
+                                    <input type="text" className="form-control" name='otherlink' placeholder="https://www.other.com/username"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Whatsapp Number*</label>
-                                <input type="number" className="form-control" name='email' placeholder="Enter Number" autoFocus />
+                                    <label className="form-label">Whatsapp Number*</label>
+                                    <input type="number" className="form-control" name='whatsapp_number' placeholder="Enter Number"  />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Gender</label>
-                                <select className="form-control" name='gender' placeholder="Enter Gender">
-                                    <option value="">Male</option>
-                                    <option value="">Female</option>
-                                </select>
+                                    <label className="form-label">Gender</label>
+                                    <select className="form-control" name='gender' placeholder="Enter Gender">
+                                        <option value="" hidden>Select Gender</option>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="other">Other</option>
+                                    </select>
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Location <span style={{fontSize:'12px',fontWeight:"600"}}>( City )</span></label>
-                                <input className='form-control' type="text" placeholder='Enter city' name='category' />
+                                    <label className="form-label">Location <span style={{fontSize:'12px',fontWeight:"600"}}>( City )</span></label>
+                                    <input className='form-control' type="text" placeholder='Enter city' name='location' />
                                 </div>
                                 <div className="form-group">
-                                <label className="form-label">Category</label>
-                                <input className='form-control' type="text" placeholder='Enter Category' name='category' />
+                                    <label className="form-label">Category</label>
+                                    <Select 
+                                        components={makeAnimated} 
+                                        options={options} 
+                                        isMulti 
+                                        onChange={(e)=>setcategory(e)} 
+                                        styles={{control:(base)=>({
+                                            ...base,height:"fit-content",padding:".375rem .2rem",border:"1px solid #dee2e6",
+                                        })}}
+                                    />
                                 </div>
                                 <div className="form-group">
-                                <button type="submit" className="btn btn-primary full-width font--bold btn-lg">Submit</button>
+                                    <button type="submit" className={`btn btn-primary full-width font--bold btn-lg ${loading ? "loading" :""}`}>
+                                        {loading?<div className="spinner-border" role="status"><span className="visually-hidden">Loading...</span></div>:"Submit"}
+                                    </button>
                                 </div>
                                 <div className="modal-flex-item d-flex align-items-center justify-content-between mb-3">
                                 </div>
